1183075f092b8edb1709785c71716e914a7c2ddb
prestonb91 <prestonb91@gmail.com>
2025-03-23 21:05:39 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Sidebar from "./components/Sidebar";
+import "./styles/utils.css";
+
+function App() {
+
+    return (
+      <>
+      <div className="bg-black h-screen">
+        <Sidebar/>
+        <div>Testing</div>
+      </div>
+      </>
+    )
+}
+
+export default App
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,20 @@
+function Sidebar() {
+    
+    return (
+        <>
+            <div
+                className="flex"
+            >
+                <div className="button">
+                    <button>About</button>
+                    <button>Experience</button>
+                    <button>Projects</button>
+                    <button>Stack</button>
+                </div>
+            </div>
+        </>
+    )
+
+}
+
+export default Sidebar
