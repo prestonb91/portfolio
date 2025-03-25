69b147f1e9c018d7bf8f4175a291b21f9c821a06
prestonb91 <prestonb91@gmail.com>
2025-03-26 00:18:15 +0900

    feat: scroll to section on button press

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,13 @@ import { useRef } from "react";
 
 function App() {
 
-  const aboutRef = useRef(null);
-  const experienceRef = useRef(null);
-  const projectsRef = useRef(null);
-  const stackRef = useRef(null);
+  const aboutRef = useRef<HTMLInputElement | null>(null);
+  const experienceRef = useRef<HTMLInputElement | null>(null);
+  const projectsRef = useRef<HTMLInputElement | null>(null);
+  const stackRef = useRef<HTMLInputElement | null>(null);
 
-
-  const scrollToSection = (ref) => {
-    ref.current.scrollIntoView({ behavior: "smooth"})
+  const scrollToSection = (ref: HTMLElement | null) => {
+    ref?.scrollIntoView({ behavior: "smooth"})
   }
 
     return (
@@ -20,6 +19,7 @@ function App() {
         {/* Sidebar */}
         <Sidebar
           scrollToSection={scrollToSection}
+          refs = {{ about: aboutRef, experience: experienceRef, projects: projectsRef, stack: stackRef }}
         />
 
         {/* Main Content */}
@@ -34,18 +34,21 @@ function App() {
           {/* Experience Section */}
           <section
             className="border-2 border-white text-white h-screen"
+            ref={experienceRef}
           >
             <h1>Experience</h1>
           </section>
           {/* Projects Section */}
           <section
             className="border-2 border-white text-white h-screen"
+            ref={projectsRef}
           >
             <h1>Projects</h1>
           </section>
           {/* Stack Section */}
           <section
             className="border-2 border-white text-white h-screen"
+            ref={stackRef}
           >
             <h1>Stack</h1>
           </section>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,14 +1,41 @@
-function Sidebar() {
-    
+import { RefObject } from "react";
+
+    interface SidebarProps {
+        scrollToSection: (element: HTMLElement | null) => void,
+        refs: {
+            about: RefObject<HTMLInputElement | null>;
+            experience: RefObject<HTMLInputElement| null>;
+            projects: RefObject<HTMLInputElement| null>;
+            stack: RefObject<HTMLInputElement| null>;
+        }
+    }
+
+function Sidebar( {scrollToSection, refs} : SidebarProps ) {
+
     return (
         <>
             <div
                 className="flex flex-col border-2 border-white w-1/10 h-screen"
             >
-                <button className="button">About</button>
-                <button className="button">Experience</button>
-                <button className="button">Projects</button>
-                <button className="button">Stack</button>
+                <button 
+                    className="button"
+                    onClick={()=>scrollToSection(refs.about.current)}
+                >About</button>
+                
+                <button 
+                    className="button"
+                    onClick={()=>scrollToSection(refs.experience.current)}
+                >Experience</button>
+                
+                <button 
+                    className="button"
+                    onClick={()=>scrollToSection(refs.projects.current)}
+                >Projects</button>
+                
+                <button 
+                    className="button"
+                    onClick={()=>scrollToSection(refs.stack.current)}
+                >Stack</button>
             </div>
         </>
     )
