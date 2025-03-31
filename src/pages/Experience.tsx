e82028ee292ab9ab1c60d73fd4f8da8c29a401b4
prestonb91 <prestonb91@gmail.com>
2025-04-01 00:09:47 +0900

    project layout

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -2,7 +2,9 @@ import { ExperienceProps } from "../types/utils"
 
 function Experience({ experienceRef } : ExperienceProps) {
 
-    const discoverNikkeiStack = ["Django", "Python", "Aws"];
+    const discoverNikkeiStack = ["Django", "Python", "AWS", "PostgreSQL"];
+    const findadocStack = ["Vue", "Typescript", "Nuxt", "Node.js", "Pinia"];
+    // const ccStack = ["React", "Typescript", "Node.js", "PostgreSQL"];
 
     return (
     <>
@@ -11,43 +13,88 @@ function Experience({ experienceRef } : ExperienceProps) {
             className="experience-container"
             ref={experienceRef}
         >
-            <h1 className="experience-header">Experience</h1>
+            <h1 className="header">Experience</h1>
             <div
                 className="experience-body"
             >
+                {/* Discover Nikke Experience */}
                 <div 
                     className="experience-card"
                 >
-                    <div>
-                        <img
-                            className="experience-card-image"
-                            src="src/assets/discover-nikkei.jpg" 
-                            alt="Profile Picture"
-                        />
-                    </div>
+                    <img
+                        className="experience-card-image"
+                        src="src/assets/discover-nikkei.jpg" 
+                        alt="Profile Picture"
+                    />
                     <div className="experience-card-bottom">
-                        <div className="experience-card-title">Title</div>
-                        <div className="experience-card-description">Description</div>
+                        <h2 className="experience-card-title">Discover Nikke</h2>
+                        <div 
+                            className="experience-card-description"
+                        >
+                            Develop and maintain community website of stories and news of Nikkei (Japanese descendants) culture.
+                        </div>
                         <div className="experience-stack-container">
                         {discoverNikkeiStack.map(stack => 
                             <div className="experience-stack">{stack}</div>
                         )}
                         </div>
                         <div className="experience-site-link-container">
-                            <div className="experience-site-link">Site Link</div>
+                            <div className="experience-site-link">Site</div>
                         </div>
                     </div>
                 </div>
+                {/* Findadoc Experience */}
                 <div 
                     className="experience-card"
                 >
-                    Card2
+                    <img
+                        className="experience-card-image"
+                        src="src/assets/findadoc.jpeg" 
+                        alt="Findadoc Image"
+                    />
+                    <div className="experience-card-bottom">
+                        <h2 className="experience-card-title">Find a Doc</h2>
+                        <div 
+                            className="experience-card-description"
+                        >
+                            Develop and maintain health service information for international community in Japan.
+                        </div>
+                        <div className="experience-stack-container">
+                        {findadocStack.map(stack => 
+                            <div className="experience-stack">{stack}</div>
+                        )}
+                        </div>
+                        <div className="experience-site-link-container">
+                            <div className="experience-site-link">Site</div>
+                        </div>
+                    </div>
                 </div>
-                <div 
+                {/* CC Experience */}
+                {/* <div 
                     className="experience-card"
                 >
-                    Card3
-                </div>
+                    <img
+                        className="experience-card-image"
+                        src="src/assets/cc.jpg" 
+                        alt="Findadoc Image"
+                    />
+                    <div className="experience-card-bottom">
+                        <h2 className="experience-card-title">Code Chrysalis</h2>
+                        <div 
+                            className="experience-card-description"
+                        >
+                            Develop and maintain health service information for international community in Japan.
+                        </div>
+                        <div className="experience-stack-container">
+                        {findadocStack.map(stack => 
+                            <div className="experience-stack">{stack}</div>
+                        )}
+                        </div>
+                        <div className="experience-site-link-container">
+                            <div className="experience-site-link">Site</div>
+                        </div>
+                    </div>
+                </div> */}
             </div>
         </div>
     </>
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -2,13 +2,90 @@ import { ProjectsProps } from "../types/utils"
 
 function Projects({ projectsRef } : ProjectsProps) {
 
+    const breezeStack = ["Django", "Python", "AWS", "PostgreSQL"];
+
     return (
     <>
         {/* Main Content */}
         <div
+            className="projects-container"
             ref={projectsRef}
         >
-            <h1>Projects</h1>
+            <h1 className="header">Projects</h1>
+            <div className="projects-body">
+                {/* Breeze */}
+                <div 
+                    className="projects-card"
+                >
+                    <img
+                        className="projects-card-image"
+                        src="src/assets/discover-nikkei.jpg" 
+                        alt="Profile Picture"
+                    />
+                    <div className="projects-card-right">
+                        <h2 className="projects-card-title">Discover Nikke</h2>
+                        <div 
+                            className="projects-card-description"
+                        >
+                            Develop and maintain community website of stories and news of Nikkei (Japanese descendants) culture.
+                        </div>
+                        <div className="projects-stack-container">
+                        {breezeStack.map(stack => 
+                            <div className="projects-stack">{stack}</div>
+                        )}
+                        </div>
+                        <div className="projects-site-link">Site</div>
+                    </div>
+                </div>
+                {/* Pelican */}
+                <div 
+                    className="projects-card"
+                >
+                    <img
+                        className="projects-card-image"
+                        src="src/assets/discover-nikkei.jpg" 
+                        alt="Profile Picture"
+                    />
+                    <div className="projects-card-right">
+                        <h2 className="projects-card-title">Discover Nikke</h2>
+                        <div 
+                            className="projects-card-description"
+                        >
+                            Develop and maintain community website of stories and news of Nikkei (Japanese descendants) culture.
+                        </div>
+                        <div className="projects-stack-container">
+                        {breezeStack.map(stack => 
+                            <div className="projects-stack">{stack}</div>
+                        )}
+                        </div>
+                        <div className="projects-site-link">Site</div>
+                    </div>
+                </div>
+                {/* Dev Search */}
+                <div 
+                    className="projects-card"
+                >
+                    <img
+                        className="projects-card-image"
+                        src="src/assets/discover-nikkei.jpg" 
+                        alt="Profile Picture"
+                    />
+                    <div className="projects-card-right">
+                        <h2 className="projects-card-title">Discover Nikke</h2>
+                        <div 
+                            className="projects-card-description"
+                        >
+                            Develop and maintain community website of stories and news of Nikkei (Japanese descendants) culture.
+                        </div>
+                        <div className="projects-stack-container">
+                        {breezeStack.map(stack => 
+                            <div className="projects-stack">{stack}</div>
+                        )}
+                        </div>
+                        <div className="projects-site-link">Site</div>
+                    </div>
+                </div>
+            </div>
         </div>
     </>
     )
